08d141799949b2aea8c43a74062bf28177e9eb6b
Shashwat Saxena <shashwat.saxena612@gmail.com>
2024-05-14 00:14:38 +0530

    init

---
+++ components/NavBar.tsx
@@ -0,0 +1,22 @@
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+  } from "@/components/ui/navigation-menu"
+  
+export default function NavBar(){
+    return <div>
+        <NavigationMenu>
+        <NavigationMenuList>
+            <NavigationMenuItem>
+                
+            </NavigationMenuItem>
+        </NavigationMenuList>
+        </NavigationMenu>
+    </div>
+}
