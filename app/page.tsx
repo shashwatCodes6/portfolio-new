5e454b46ad7895613e417252ac5a6260781ef7c4
Shashwat Saxena <shashwat.saxena612@gmail.com>
2024-05-16 23:42:39 +0530

    some slow progress

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,17 @@ export default function Home() {
   return (
     <div>
       <NavBar />
+      <div className="m-5 flex justify-around">
+        <div className="my-10 mx-5 align-center text-xl">
+          Hi! I am Shashwat
+          <div>
+            I am a developer, problem solver and keen learner
+          </div>
+        </div>
+        <div className="m-10 hidden md:block">
+          pic goes here
+        </div>
+      </div>
     </div>
   );
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,22 +1,21 @@
-import {
-    NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuIndicator,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-    NavigationMenuViewport,
-  } from "@/components/ui/navigation-menu"
-  
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+
+
 export default function NavBar(){
-    return <div>
-        <NavigationMenu>
-        <NavigationMenuList>
-            <NavigationMenuItem>
-                
-            </NavigationMenuItem>
-        </NavigationMenuList>
-        </NavigationMenu>
-    </div>
+    return( 
+        <div className="flex justify-between pd-2 m-4">
+            <div className="text text-3xl font-bold m-2">
+                Shashwat
+            </div>
+            <div className="m-1 hidden md:block">
+                <Button size="lg" variant="ghost">
+                    <Link href="/login">Projects</Link>
+                </Button>
+                <Button size="lg" variant="ghost">
+                    <Link href="/login">Acheivements</Link>
+                </Button>
+            </div>
+        </div>
+    )
 }
