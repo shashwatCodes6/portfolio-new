b7abfbc3716c9d44c29443a3ca0954bac43d9458
Shashwat Saxena <shashwat.saxena612@gmail.com>
2024-05-22 00:57:38 +0530

    beautified carosel

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -4,14 +4,14 @@ import image from '../public/Zhy8h3T5_400x400.jpeg'
 export default function Intro(){
     return (
       <div className="h-screen flex-row">
-        <div className="p-24 flex sm:flex-col lg:flex-row justify-around">
+        <div className="p-24 flex flex-col lg:flex-row justify-around">
         <div className="align-center text-xl m-20">
           Hi! I am Shashwat
           <div className=''>
             I am a developer, problem solver and keen learner
           </div>
         </div>
-        <div className="my-5  m-20">
+        <div className="my-5 m-20 flex justify-center">
           <Image 
             src={image}
             width={300}
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -12,32 +12,28 @@ import {
 export default function Projects(){
     return (
     <div className="bg-slate-400 flex flex-col col-span-1">
-        <div className="m-10 text text-3xl font-bold ">
+        <div className="m-10 text text-3xl font-bold flex justify-center">
           Projects
         </div>
-      <div className="flex justify-center">
-          <Carousel
-        opts={{
-          align: "start",
-        }}
-        className="w-full max-w-sm"
-      >
-        <CarouselContent>
-          {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-              <div className="p-1">
-                <Card>
-                  <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-3xl font-semibold">{index + 1}</span>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
+      <div className="flex justify-center m-10">
+
+        <Carousel className="w-full max-w-xs">
+          <CarouselContent>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index}>
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                      <span className="text-4xl font-semibold">{index + 1}</span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </div>
       </div>
     )
